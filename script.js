5509fe0c0ab3f939b6d06a0b772d1a32ca57d79a
skyrisevisuals <skyrisevisuals@gmail.com>
2026-08-05 11:45:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 const hiddenElements = document.querySelectorAll(
-".hidden, .hidden-left, .hidden-right, .hidden-scale"
+    ".hidden, .hidden-left, .hidden-right"
 );
+
 
 const observer = new IntersectionObserver((entries)=>{
 
     entries.forEach((entry)=>{
 
         if(entry.isIntersecting){
+
             entry.target.classList.add("show");
+
+            observer.unobserve(entry.target);
+
         }
 
     });
 
 },{
+
     threshold:.15
+
 });
 
+
 hiddenElements.forEach((el)=>{
+
     observer.observe(el);
+
 });
